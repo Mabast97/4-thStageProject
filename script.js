d5d4636d8ac7880a5c7cc7f3ca0576ea36c8ee66
Mabast <mabasthashm1@gmail.com>
2018-10-28 00:12:03 +0300

    version 0.1

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed("#welcome-text", {
+    strings: [
+    "Hello World",
+    "Holla World",
+    "Hey Mabast",
+    "Welcome To Here"
+    ],
+    startDelay: 300,
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    showCursor: false,
+    // cursorChar: '/',
+    loop: true
+    });
